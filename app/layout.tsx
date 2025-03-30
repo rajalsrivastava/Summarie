553c375911958f4bee809ba777c69fd4df43b633
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-03-31 04:38:33 +0530

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight:['200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
   title: "Summarie - AI-powered PDF Summarization",
-  description: "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+  description:
+    "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
 };
 
 export default function RootLayout({
@@ -20,10 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} font-sans antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
