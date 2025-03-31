13fb4ef173eb1a11830e4cffe0434f7c496203a5
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-04-01 04:11:53 +0530

    add how-it-works section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import BgGradient from "@/components/common/bg-gradient";
 import HeroSection from "@/components/home/hero-section";
 import DemoSection from "@/components/home/demo-section";
-
+import HowItWorksSection from "@/components/home/how-it-works-section";
 export default function Home() {
   return (
     <div className="relative w-full">
@@ -9,8 +9,8 @@ export default function Home() {
       <div className="flex flex-col ">
         <HeroSection />
         <DemoSection />
+        <HowItWorksSection />
       </div>
-      {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
