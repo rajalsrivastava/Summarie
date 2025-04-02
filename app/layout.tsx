b9973dd289971623c4ac94154afac45042428b9a
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-04-03 05:27:09 +0530

    implement summary list page and delete summary functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
-        <Toaster />
+        <Toaster richColors/>
       </body>
     </html>
     </ClerkProvider>
